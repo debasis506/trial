688c6a34f0c5e8f69a91dcbb3342fa3aabe25f4e
unknown <debasis506@gmail.com>
2023-01-16 22:29:32 +0530

    evenorodd

---
+++ temp.js
@@ -0,0 +1,17 @@
+  
+  let obj={
+  for(let i=0;i<n;i++){
+      if(obj[arr[i]]==undefined){
+          obj[arr[i]]=1
+      }
+  }
+  let count=0
+  let bag=""
+  for(let key in obj){
+      if(obj[key]==1){
+          bag+=key+" "
+          count++
+      }
+  }
+  console.log(count)
+  console.log(bag)
